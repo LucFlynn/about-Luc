03fff7b4c28c094c7e20f8ea63f99d24b5e3f13f
Emily Halleran <emily@blueroserevival.com>
2017-04-10 13:24:43 -0600

    Test Minor Change

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 		<title>"About"</title>
 	</head>
 		<body>
-			<img src="images/luc.png" height="250" width="200" alt="about-luc">
+			<img src="images/luc.png" height="550" width="450" alt="about-luc">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/U9t-slLl30E" frameborder="0" allowfullscreen></iframe>
 		<p>
 			This is the page about me! My favorite subject. And you could see my face if I could figure it out.
 		</p>
