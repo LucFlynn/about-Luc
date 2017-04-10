30d9a20fa01e3cfbeda074bc386406dbd6023a23
Luc Flynn <lucnmsu@gmail.com>
2017-04-10 11:38:38 -0600

    getting pic to work

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<title>"About"</title>
 	</head>
 		<body>
-			<p> img src="images/about-luc.jpg" alt="about-luc"></p>
+			<p><img src="images/about-luc.jpg" alt="about-luc"></p>
 		<p>
 			This is the page about me! My favorite subject.
 		</p>
