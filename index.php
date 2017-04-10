05c60ad4ad0111d4564ba29b41022fbdfa36d2ce
Emily Halleran <emily@blueroserevival.com>
2017-04-10 14:12:53 -0600

    Test Minor Change

--- index.php
+++ index.php
@@ -14,9 +14,12 @@
 			<h2>Recently I had an encounter with a tiny green man. He warned me of the seagulls. He also told me about
 				the duck woman hiding in the bushes who killed my father. I wanted to share this documentary I created
 				as a PSA announcement for all creatures in this universe and the next.</h2>
-			<p>I think it very important to know your MBTI type. Mine is obviously the INFP. The enigma. The unwilling hero</p>
+			<p>I think it very important to know your MBTI type. Mine is obviously the INFP. The enigma. The unwilling hero.</p>
 			<img src="images/fullofthemselvesinfps.jpg" alt="we are the best">
 			<img src="images/infp.jpg" alt="don't look at me">
+			<p> I used to watch Star Trek growing up. Much of what I am today is compiled of what I saw in that fascinating show. My favorite character is Picard. Because he
+			is almost as valiant as I am. But not quite.</p>
+			<img src="images/picard.jpg" alt="mylove">
 
 	</body>
 
