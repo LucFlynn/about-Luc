9f08959b658e9f74d1ad0bb298bc338ea3dcfd60
Emily Halleran <emily@blueroserevival.com>
2017-04-10 13:21:03 -0600

    Test Minor Change

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title>"About"</title>
 	</head>
 		<body>
-			<img src="images/luc.png" alt="about-luc">
+			<img src="images/luc.png" height="250" width="200" alt="about-luc">
 		<p>
 			This is the page about me! My favorite subject. And you could see my face if I could figure it out.
 		</p>
