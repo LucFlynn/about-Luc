48c17dc59a68736adeb99d7f399c21f68b69d1a7
Luc Flynn <lucnmsu@gmail.com>
2017-04-10 12:54:18 -0600

    getting pic to work

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<body>
 			<p><img src="images/about-luc.jpg" alt="about-luc"></p>
 		<p>
-			This is the page about me! My favorite subject.
+			This is the page about me! My favorite subject. And you could see my face if I could figure it out.
 		</p>
 	</body>
 
